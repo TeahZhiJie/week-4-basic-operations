a080e5aa33d833d1e8d8f77386555e46f625725d
TEAH ZHI JIE <93848358+TeahZhiJie@users.noreply.github.com>
2022-04-18 10:22:23 +0800

    Update exercise6.c

--- exercise6.c
+++ exercise6.c
@@ -1,19 +1,17 @@
-//documentation section 
-/* Exercise 6 - Deletion Operations */
-
-//pre-processor section 
 #include<stdio.h>
 
-//global variable section 
+int main()
+{
+    float LA[] = {1.5, 1.2, 1.8, 1.6, 1.1};
 
+    for (int i = 2; i < 3; i++)
+    {
+        LA[i] = LA[i+2]; 
+    }
 
-//main function section 
-int main(){
-    //write here your program
-
-
-    return 0;
+    printf("Output: \n"); 
+    for (int i = 0; i < 3; i++)
+    {
+        printf("LA[%d] = %.1f\n", i, LA[i]); 
+    }
 }
-
-
-//user-defined section 
