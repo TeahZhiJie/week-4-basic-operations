31c18dc19517283e45a90c8398ee0b83fa6161b8
TEAH ZHI JIE <93848358+TeahZhiJie@users.noreply.github.com>
2022-04-18 10:23:20 +0800

    Update exercise8.c

--- exercise8.c
+++ exercise8.c
@@ -1,19 +1,19 @@
-//documentation section 
-/* Exercise 8 - Search Operations */
-
-//pre-processor section 
 #include<stdio.h>
 
-//global variable section 
+int main()
+{
+    int LA[3][2] = {{1,2}, {3,4}, {5,6}}; 
 
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            if (LA[i][j] == 5)
+            {
+                printf("Output: \n"); 
+                printf("Found value 5 at position row %d column %d \n", i+1, j+1); 
 
-//main function section 
-int main(){
-    //write here your program
-
-
-    return 0;
+            }
+        }
+    }
 }
-
-
-//user-defined section 
